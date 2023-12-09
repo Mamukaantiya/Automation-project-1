b7abd4f9ac6e4b1ef682d7f01ec4e68fe92617ce
Mamukaantiya <149116322+Mamukaantiya@users.noreply.github.com>
2023-12-09 12:47:27 +0200

    Add files via upload

--- issue-comments.cy.js
+++ issue-comments.cy.js
@@ -68,4 +68,60 @@ describe('Issue comments creating, editing and deleting', () => {
             .find('[data-testid="issue-comment"]')
             .should('not.exist');
     });
-});
+
+    it('Automation test that combines the existing tests', () => {
+        const comment = 'Christmas is coming';
+        const editedComment = 'We wish you a merry christmas'
+
+        getIssueDetailsModal().within(() => {
+            cy.contains('Add a comment...')
+                .click();
+
+            cy.get('textarea[placeholder="Add a comment..."]').type(comment);
+
+            cy.contains('button', 'Save')
+                .click()
+                .should('not.exist');
+
+            cy.contains('Add a comment...').should('exist');
+            cy.get('[data-testid="issue-comment"]').should('contain', comment);
+        });    
+
+        getIssueDetailsModal().within(() => {
+            cy.get('[data-testid="issue-comment"]')
+                .first()
+                .contains('Edit')
+                .click()
+                .should('not.exist');
+
+            cy.get('textarea[placeholder="Add a comment..."]')
+                .should('contain', comment)
+                .clear()
+                .type(editedComment);
+
+            cy.contains('button', 'Save')
+                .click()
+                .should('not.exist');
+
+            cy.get('[data-testid="issue-comment"]')
+                .should('contain', 'Edit')
+                .and('contain', editedComment);
+            });
+
+        getIssueDetailsModal()
+                .find('[data-testid="issue-comment"]')
+                .contains('Delete')
+                .click();
+
+            cy.get('[data-testid="modal:confirm"]')
+                .contains('button', 'Delete comment')
+                .click()
+                .should('not.exist');
+
+        getIssueDetailsModal()
+                .find('[data-testid="issue-comment"]')
+                .contains(comment)
+                .should('not.exist');
+        });
+    });
+    
